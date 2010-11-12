2ab4f482c4c9163cabc4818440c614c2b5c7b06b
Rares Mirica <mrares@gmail.com>
2010-11-12 13:45:19 -0800

    First commit

---
+++ src/NodeMod.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : NodeMod.cpp
+// Author      : Mrares
+// Version     :
+// Copyright   : COPY-ME!
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
