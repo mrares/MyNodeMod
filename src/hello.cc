fd3980fa7b421c800c2fbe11d1a48a2f67dce727
mrares <mrares@mrares-virtual-machine>
2010-11-19 12:39:53 +0200

    Commented a line

--- src/hello.cc
+++ src/hello.cc
@@ -10,7 +10,7 @@ init (Handle<Object> target)
 //  target->Set(0, String::New("Hello"));
 //  target->Set(1, String::New("world!"));
 //  target->Set(Int32::New(1),String::New('World'));
-  target->Set(String::New("hello"), String::New("World"));
+//  target->Set(String::New("hello"), String::New("World"));
   target->Set(String::New("Atlantic"), String::New("Ocean"));
 }
 
